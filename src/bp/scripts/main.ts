8c460595a5f626529065751fac12e55c7d1fe9f8
LuckedCoronet <lc45@lc-studios.net>
2025-10-11 07:45:48 +0900

    Update scripts

--- src/bp/scripts/main.ts
+++ src/bp/scripts/main.ts
@@ -1,4 +1,4 @@
 // This is the entry point script, whose only purpose is to import other
 // modules that contain the actual business logic.
 
-import "./core/slasher/slasher";
+import "./core/slasher";

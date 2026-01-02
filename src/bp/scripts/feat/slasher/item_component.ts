3f6a22b59027e00f7557061b0c35fdcbc81f80b7
LuckedCoronet <lc45@lc-studios.net>
2026-01-02 09:39:24 +0900

    remove slasher power cell custom item component

--- src/bp/scripts/feat/slasher/item_component.ts
+++ src/bp/scripts/feat/slasher/item_component.ts
@@ -7,20 +7,3 @@ mc.system.beforeEvents.startup.subscribe((e) => {
 		},
 	});
 });
-
-mc.system.beforeEvents.startup.subscribe((e) => {
-	e.itemComponentRegistry.registerCustomComponent("shr:slasher_power_cell", {
-		onUse({ source, itemStack }) {
-			if (!source) return;
-
-			const equippable = source.getComponent("equippable")!;
-			const mainhandItem = equippable.getEquipment(mc.EquipmentSlot.Mainhand);
-			const offhandItem = equippable.getEquipment(mc.EquipmentSlot.Offhand);
-
-			if (mainhandItem?.type !== itemStack?.type) return;
-
-			equippable.setEquipment(mc.EquipmentSlot.Offhand, itemStack);
-			equippable.setEquipment(mc.EquipmentSlot.Mainhand, offhandItem);
-		},
-	});
-});
